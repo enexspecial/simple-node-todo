9d3edf89d4d4b8200d730518be1a2a12f6cb48a5
John Henry <john04star@gmail.com>
2021-08-03 10:17:40 +0100

    updated the views

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res)=>{
-    res.sendFile('../views/index.html', {root: __dirname})
+    res.sendFile('./views/index.html', {root: __dirname})
 });
 
-app.get('/add0-item', (req, res)=>{
-    res.sendFile('../views/add-items.html', {root: __dirname})
+app.get('/add-items', (req, res)=>{
+    res.sendFile('./views/add-items.html', {root: __dirname})
+});
+
+app.use((req, res)=>{
+    res.sendFile('./views/404.html', {root: __dirname})
 })
 
